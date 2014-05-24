1dcf4cabc67c4a7657a4de7b91d8788e99033c4b
HagitKL <hagit.kornreich@gmail.com>
2014-05-24 16:27:44 -0400

    initial changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,31 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##      If the inverse has already been calculated (and the matrix has not changed), 
+##      then the cachesolve should retrieve the inverse from the cache.
+##      functions do
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(length(x),2,2)) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+       }
+        cacheSolve <- function(x, ...) {
+                ## Return a matrix that is the inverse of 'x'
+                xinvert<-solve(x)
+                xinvert
+                
+        }
+        get <- function() x
+        setinvert <- function(xinvert) m <<- xinvert
+        getinvert <- function() m
+        list(set = set, get = get,
+             setinvert = setinvert,
+             getinvert = getinvert)
+        print(set,get,setinvert, getinvert)
 }
 
 
@@ -12,4 +33,30 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
